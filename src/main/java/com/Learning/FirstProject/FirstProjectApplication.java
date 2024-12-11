fa7f62762c7235012b9615d7ef16e6679e74c1b0
Ahmed <Ahmed_hazem.atia@hotmail.com>
2024-12-11 14:31:38 +0200

    that's fixing the missing bean of mapStruct

--- src/main/java/com/Learning/FirstProject/FirstProjectApplication.java
+++ src/main/java/com/Learning/FirstProject/FirstProjectApplication.java
@@ -3,7 +3,7 @@ package com.Learning.FirstProject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.Learning.FirstProject")
 public class FirstProjectApplication {
 
 	public static void main(String[] args) {
